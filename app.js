265b45e00a46da6d96584300fca77c404161a984
iabdulhakim <abdulhakimisroil@gmail.com>
2024-01-09 23:39:59 +0500

    fixed some bugs

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-function myFunction() {
-    let element = document.body;
-    let divEl = document.div;
-    element.classList.toggle("dark-theme");
-    divEl.classList.toggle("dark-mode");
-};
 
+// fetch("./data.json")
+// .then((response) => response.json())
+// .then((data) => {
+//     console.log(data);
 
-const switchEl = document.querySelector(".modeSwitcher");
+//     function createCard(job) {
+//         const card =document.createElement("div");
+//         card.classList.add("yashiq");
 
-switchEl.addEventListener("click", ()=> {
-    switchEl.setAttribute("src", "./Pictures/mode-switcher-on.svg");
-});
-
-
-
-// const theme = document.getElementById("toggleBtn");
-
-// theme.onclickn = function() {
-//     document.body.classList.toggle("dark-theme");
-// };
+//         card.innerHTML = `
+//         <${job.logoBackground}>
+//         <img src="${job.logo}" alt="" />
+//         <div class="yashiq_con">.
+//             <p>${job.postedAt} . ${job.contract}</p>
+//             <h3><a href='${job.website}'>${job.position}</a></h3>
+//             <p>${job.company}</p>
+//             <h6>${job.location}</h6>
+//             </div>
+//             `;
+//         return card;
+//     }
+// });
