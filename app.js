48de5f4b81b3c436369a184385da101107b28853
iabdulhakim <abdulhakimisroil@gmail.com>
2024-01-09 19:05:47 +0500

    fixed bugs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function myFunction() {
     let element = document.body;
     let divEl = document.div;
-    element.classList.toggle("dark-mode");
+    element.classList.toggle("dark-theme");
     divEl.classList.toggle("dark-mode");
 };
 
@@ -12,3 +12,10 @@ switchEl.addEventListener("click", ()=> {
     switchEl.setAttribute("src", "./Pictures/mode-switcher-on.svg");
 });
 
+
+
+// const theme = document.getElementById("toggleBtn");
+
+// theme.onclickn = function() {
+//     document.body.classList.toggle("dark-theme");
+// };
